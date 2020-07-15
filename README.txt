3ebf5034205f62cb86806eafec437aaba9089d1c
guidanoli <guidanoli@hotmail.com>
2020-07-15 19:43:17 -0300

    Initial commit

---
+++ README.txt
@@ -0,0 +1,12 @@
+Chess 99
+--------
+
+This is intended to be a translation of the chesslib library
+(guidanoli/chess @ Github) to ANSI C or C99.
+
+This will be a great challenge since much of the functionalities
+were implemented using C++ features such as classes, operator
+overloading and much of the standard library data structures.
+
+At the same time, much learning will come into play into this
+exciting adventure.
---
+++ tests/test_manager.c
@@ -0,0 +1,65 @@
+#include "test_manager.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+struct stack
+{
+	struct stack* next;
+	const void* info;
+};
+
+size_t succeeded = 0;
+size_t failed = 0;
+struct stack* name_stack = NULL;
+
+size_t get_test_id()
+{
+	return succeeded + failed;
+}
+
+void print_test_name()
+{
+	printf("Test ");
+	for (struct stack* name = name_stack; name != NULL; name = name->next)
+		printf("%s:", (const char*) name->info);
+	printf("#%zu", get_test_id());
+}
+
+void test_succeeded()
+{
+	++succeeded;
+	print_test_name();
+	printf(" succeeded\n");
+}
+
+void test_failed()
+{
+	++failed;
+	print_test_name();
+	printf(" failed\n");
+}
+
+void push_name(const char* name)
+{
+	struct stack* new_name = malloc(sizeof(struct stack));
+	new_name->next = name_stack;
+	new_name->info = name;
+	name_stack = new_name;
+}
+
+void pop_name()
+{
+	struct stack* top = name_stack;
+	name_stack = top->next;
+	free(top);
+}
+
+void print_results()
+{
+	if (failed == 0)
+		printf("All %zu tests passed\n", succeeded);
+	else
+		printf("%zu from %zu tests failed\n", failed, succeeded + failed);
+}
