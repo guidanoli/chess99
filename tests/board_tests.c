87c5e1af3e4ee1de94ad81c27badc4c5708d6a70
guidanoli <guidanoli@hotmail.com>
2020-07-18 13:11:18 -0300

    Added names to board_tests, fixed gt/01.dat and added gt/13.dat (both for game_state_tests.c)

--- tests/board_tests.c
+++ tests/board_tests.c
@@ -5,20 +5,34 @@
 
 void run_board_tests()
 {
-	test_manager_push_name("BoardTest");
+	test_manager_push_name("board.h");
 
 	Board* b = Board_new();
 
+	test_manager_push_name("Board_check");
+
 	assert(Board_check(b));
-	assert(utils_gt((utils_gt_func) Board_print, b, "02.dat"));
-	assert(utils_gt((utils_gt_func) Board_save, b, "03.dat"));
+	
+	test_manager_pop_name(); // Board_check
+	
+	test_manager_push_name("Board_at");
+
 	assert(Board_at(b, SQ_A1)->type_id == PTID_ROOK);
 	assert(Board_at(b, SQ_A2)->type_id == PTID_PAWN);
 	assert(Board_at(b, SQ_A3)->type_id == PTID_EMPTY);
 	assert(Board_at(b, SQ_A1)->colour == COLOUR_WHITE);
 	assert(Board_at(b, SQ_A8)->colour == COLOUR_BLACK);
+	
+	test_manager_pop_name(); // Board_at
+	
+	test_manager_push_name("GT");
+
+	assert(utils_gt((utils_gt_func) Board_print, b, "02.dat"));
+	assert(utils_gt((utils_gt_func) Board_save, b, "03.dat"));
+	
+	test_manager_pop_name(); // GT
 
 	Board_delete(b);
 
-	test_manager_pop_name();
+	test_manager_pop_name(); // Board.h
 }
--- tests/game_state_tests.c
+++ tests/game_state_tests.c
@@ -1,26 +1,40 @@
+#include "utils.h"
 #include "tests.h"
 #include "test_manager.h"
 #include "state.h"
 
 void run_game_state_tests()
 {
-	test_manager_push_name("GameStateTests");
+	test_manager_push_name("state.h");
 
 	GameState* g = GameState_new();
 
-	// Check initial state
+	test_manager_push_name("InitialState");
+
 	assert(GameState_check(g));
 	assert(GameState_getTurn(g) == COLOUR_WHITE);
 	assert(GameState_getPhase(g) == PHASE_RUNNING);
 	assert(GameState_getEnPassant(g) == EN_PASSANT_NONE);
 
-	// Switch turns	
+	test_manager_pop_name(); // InitialState
+
+	test_manager_push_name("GT");
+
+	assert(utils_gt((utils_gt_func) GameState_save, g, "01.dat"));
+
+	test_manager_pop_name(); // GT
+
+	test_manager_push_name("Turn");
+
 	GameState_nextTurn(g);
 	assert(GameState_getTurn(g) == COLOUR_BLACK);
 	GameState_nextTurn(g);
 	assert(GameState_getTurn(g) == COLOUR_WHITE);
 
-	// Switch phase
+	test_manager_pop_name(); // Turn
+
+	test_manager_push_name("Phase");
+
 	GameState_setPhase(g, PHASE_WHITE_WON);
 	assert(GameState_getPhase(g) == PHASE_WHITE_WON);
 	GameState_setPhase(g, PHASE_BLACK_WON);
@@ -28,30 +42,50 @@ void run_game_state_tests()
 	GameState_setPhase(g, PHASE_RUNNING);
 	assert(GameState_getPhase(g) == PHASE_RUNNING);
 
-	// Move a piece to an empty square
+	test_manager_pop_name(); // Phase
+
+	test_manager_push_name("MoveToEmpty");
+
 	GameState_movePiece(g, SQ_A2, SQ_A4);
 	assert(GameState_getPieceAt(g, SQ_A2)->type_id == PTID_EMPTY);
 	assert(GameState_getPieceAt(g, SQ_A4)->type_id == PTID_PAWN);
 	assert(GameState_getPieceAt(g, SQ_A4)->colour == COLOUR_WHITE);
 
-	// Move a piece to another piece
+	test_manager_pop_name(); // MoveToEmpty
+
+	test_manager_push_name("MoveToPiece");
+	
 	GameState_movePiece(g, SQ_A1, SQ_A8);
 	assert(GameState_getPieceAt(g, SQ_A1)->type_id == PTID_EMPTY);
 	assert(GameState_getPieceAt(g, SQ_A8)->type_id == PTID_ROOK);
 	assert(GameState_getPieceAt(g, SQ_A8)->colour == COLOUR_WHITE);
 
-	// Clear an empty square
+	test_manager_pop_name(); // MoveToPiece
+
+	test_manager_push_name("ClearEmpty");	
+
 	assert(GameState_getPieceAt(g, SQ_A3)->type_id == PTID_EMPTY);
 	GameState_clearSquare(g, SQ_A3);
 	assert(GameState_getPieceAt(g, SQ_A3)->type_id == PTID_EMPTY);
 
-	// Clear a square with a piece
+	test_manager_pop_name(); // ClearEmpty
+	
+	test_manager_push_name("ClearPiece");
+
 	assert(GameState_getPieceAt(g, SQ_B1)->type_id == PTID_KNIGHT);
 	assert(GameState_getPieceAt(g, SQ_B1)->colour == COLOUR_WHITE);
 	GameState_clearSquare(g, SQ_B1);
 	assert(GameState_getPieceAt(g, SQ_B1)->type_id == PTID_EMPTY);
 
-	GameState_delete(g);
+	test_manager_pop_name(); // ClearPiece
+
+	test_manager_push_name("GT");
+
+	assert(utils_gt((utils_gt_func) GameState_save, g, "13.dat"));
 
 	test_manager_pop_name();
+
+	GameState_delete(g);
+
+	test_manager_pop_name(); // state.h
 }	
